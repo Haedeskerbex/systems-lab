bbbea9ba927cc4f91e5a052f07898c27c7db4f2b
Arjun Anil <arjunanilcollab@gmail.com>
2025-04-18 16:46:33 +0530

    added module param

--- hello.c
+++ hello.c
@@ -1,14 +1,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
 
-int init_module(void) {
-	printk(KERN_INFO "Hello, Kernel World!\n");
+int myint = 7;
+module_param(myint,int,0);
+MODULE_PARM_DESC(myint, "An Integer");
+static int __init hello_init(void) {
+	printk(KERN_INFO "Hello, myint = %d\n", myint);
 	return 0;
 }
 
-void cleanup_module(void){
-	printk(KERN_INFO "Goodbye Kernel World!\n");
+static void __exit hello_exit(void){
+	printk(KERN_INFO "Goodbye, myint = %d\n", myint);
 }
+module_init(hello_init);
+module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
 
